181ba935fa1bf5c4dc8f15cea30783a8c0f49bbc
deminkmnn <148581675+deminkmnn@users.noreply.github.com>
2024-10-22 10:28:44 +0000

    Delete src directory

--- src/mx_memalloc_zero.c
+++
@@ -1,9 +0,0 @@
-#include "../inc/pathfinder.h"
-
-void *mx_memalloc_zero(size_t size) {
-    void *ptr = malloc(size); 
-    if (ptr) {
-        mx_memset(ptr, '\0', size); 
-    }
-    return ptr; 
-}
